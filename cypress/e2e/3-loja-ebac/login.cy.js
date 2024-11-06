bd33d4a819094aeac596c8001a8da4db89a3853e
Renata Nogueira <re.nogueira@yahoo.com.br>
2024-11-06 20:36:21 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,58 @@
+/// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
+
+
+describe('Funcionalidade: Login', () => {
+
+    beforeEach(() => {
+        cy.visit('minha-conta')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () => {        
+        cy.get('#username').type('renata.teste@teste.com.br')
+        cy.get('#password').type('Teste123@')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, renata.teste (não é renata.teste? Sair)')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('renata@teste.com.br')
+        cy.get('#password').type('Teste123@')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve exibir mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('renata.teste@teste.com.br')
+        cy.get('#password').type('Teste124@')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail renata.teste@teste.com.br está incorreta.')
+        cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, renata.teste1 (não é renata.teste1? Sair)')
+    });
+
+    it('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then( dados => {
+            cy.get('#username').type(dados.usuario, {log:false})
+            cy.get('#password').type(dados.senha, {log:false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, renata.teste1 (não é renata.teste1? Sair)')
+        })
+    });
+
+    it.only('Deve fazer login com sucesso - usando Comandos customizado', () => {
+        cy.login('renata.teste1@teste.com.br', 'Teste123@')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, renata.teste1 (não é renata.teste1? Sair)')
+        
+    });
+})
